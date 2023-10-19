351c71b5cde749b9a5cf70c633952b878d5a8855
TCP-ENG <129853844+Travis-DM@users.noreply.github.com>
2023-10-19 14:24:57 -0400

    initial commit

---
+++ led.cpp
@@ -0,0 +1,9 @@
+#include <Arduino.h>
+//#include "led.h"
+//#include <WS2812Serial.h>
+
+
+
+
+
+
---
+++ led.h
@@ -0,0 +1,39 @@
+#ifndef _LED_H_
+#define _LED_H_
+
+#include <WS2812Serial.h>
+
+bool ledEN0(void);
+bool ledEN1(void);
+bool ledEN2(void);
+bool ledEN3(void);
+
+
+#define numled 64
+#define pin 17
+
+
+
+void setLED(int x,int color);
+class LED
+{
+private:
+    bool enabled;
+    int lpin;
+    uint8_t pID;
+    uint8_t ledCount;
+
+public:
+    LED(/* args */);
+    ~LED();
+    void Setup(int s_pin, uint8_t s_pID, uint8_t mode, uint8_t s_ledCount, uint8_t red, uint8_t blue, uint8_t green);
+    void setLEDs(int color);
+    void setLEDs(uint8_t red, uint8_t blue, uint8_t green);
+    void setLED(uint8_t red, uint8_t blue, uint8_t green);
+};
+
+LED led0();
+LED led1();
+LED led2();
+
+#endif
